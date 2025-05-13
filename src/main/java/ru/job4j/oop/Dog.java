0d9681edaac8307266a95a434bee4233de4c5e4d
lehade <alexey.denisenko@me.com>
2025-05-13 21:43:06 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog sharik = new Dog();
+        Dog polkan = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
