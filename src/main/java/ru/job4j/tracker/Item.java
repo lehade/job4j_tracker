07de39053b83dbfde50f637d255340d2c47856fb
lehade <alexey.denisenko@me.com>
2025-08-06 22:52:11 +0300

    Перегрузка конструктора [#173890 #549692]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -12,24 +12,24 @@ public class Item {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public Item() {
-
-    }
-
     public Item(String name) {
-
+        this.name = name;
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Item() {
+
     }
 }
